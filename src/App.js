b4b73c6d10dff460376400a926c1519b7580c94f
jarabeenoi <nongnat61@gmail.com>
2020-10-13 16:50:37 +0700

    Dice game V.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 
 import Header from './components/Header';
@@ -6,13 +6,56 @@ import ScoreCard from './components/ScoreCard';
 import DiceCard from './components/DiceCard';
 
 function App() {
+  const [playerScore, setPlayerscore] = useState([0,0]);
+  const [currenPlayer, setCurrenPlayer] = useState(0);
+  const [diceScore, setDiceScore] = useState(1);
+  const [roundScore, setRoundScore] = useState(0);
+
+  const roll = () => {
+    const newDiceScore = Math.ceil(Math.random() * 6);
+    setDiceScore(newDiceScore);
+    if (newDiceScore === 1) {
+      setCurrenPlayer(currenValue => currenValue === 0 ? 1 : 0);
+      setRoundScore(0);
+    } else {
+      setRoundScore(currenValue => currenValue + newDiceScore);
+    }
+  };
+
+  const keep = () => {
+    setPlayerscore(currenValue => 
+      currenValue.map((ele, idx) => idx === currenPlayer ? ele + roundScore : ele))
+    setCurrenPlayer(currenValue => currenValue === 0 ? 1 : 0);
+    setRoundScore(0);
+  };
+
+  const reset = () => {
+    setPlayerscore([0, 0]);
+    setCurrenPlayer(0);
+    setDiceScore(1);
+    setRoundScore(0);
+  }
+
+
   return (
     <Fragment>
-      <Header />
+      <Header reset={reset} />
       <div className="container">
-        <ScoreCard />
-        <DiceCard />
-        <ScoreCard />
+        <ScoreCard 
+        name="1" 
+        score={playerScore[0]} 
+        active={currenPlayer === 0 ? true: false} />
+
+        <DiceCard 
+        diceScore={diceScore}
+        roundScore={roundScore}
+        roll={roll} 
+        keep={keep}/>
+
+        <ScoreCard 
+        name="2" 
+        score={playerScore[1]} 
+        active={currenPlayer === 1 ? true: false} />
       </div>
     </Fragment>
   );
